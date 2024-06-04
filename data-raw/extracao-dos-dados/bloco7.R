f55d9e37e6c34d3817c508b4111c2a64cbd018a7
marianacfreitas <122453291+marianacfreitas@users.noreply.github.com>
2024-06-04 00:04:00 -0300

    Add files via upload

--- data-raw/extracao-dos-dados/bloco7.R
+++ data-raw/extracao-dos-dados/bloco7.R
@@ -7,8 +7,8 @@ tabela_aux_municipios <- read.csv("data-raw/extracao-dos-dados/databases-antigas
 
 ############## INDICADORES DA ABA FETAL
 
-df_fetais_totais <- fetch_datasus(
-  year_start = 2012,
+df_fetais_totais1 <- fetch_datasus(
+  year_start = 2022,
   year_end = 2022,
   vars = c("CODMUNRES", "DTOBITO",
                   "PESO",
@@ -17,6 +17,13 @@ df_fetais_totais <- fetch_datasus(
                   "OBITOPARTO"),
   information_system = "SIM-DOFET"
 )
+
+sim_2023 <- fread("https://s3.sa-east-1.amazonaws.com/ckan.saude.gov.br/SIM/DO23OPEN.csv")
+
+sim_dofet2023 <- sim_2023 |> filter(TIPOBITO == 1) |>
+  select(c(CODMUNRES, DTOBITO, PESO, GESTACAO, SEMAGESTAC, OBITOPARTO))
+
+df_fetais_totais <- rbind(df_fetais_totais1, sim_dofet2023)
 
 df_fetais_totais2 <- process_sim(df_fetais_totais, municipality_data = TRUE)
 
@@ -35,10 +42,14 @@ dados_fetais <- df_fetais_totais2 |>
 
 df_fetais <- dados_fetais |>
   mutate(
-    ano = substr(DTOBITO, 1, 4),
+    ano1 = substr(DTOBITO, 1, 4),
     codmunres = CODMUNRES
   ) |>
   mutate(
+    ano = case_when(
+      ano1 == "23" ~ "2023",
+      ano1 != "23" ~ ano1
+    ),
     obitos= 1,
     peso_menos_1500 = case_when(
       PESO < 1500 ~ 1,
@@ -203,6 +214,7 @@ df_fetais <- dados_fetais |>
     # )
 
   ) |>
+  select(-c(ano1))|>
   group_by(codmunres, ano) |>
   summarise(
 
@@ -253,10 +265,10 @@ df_fetais <- dados_fetais |>
   ungroup()
 
 codigos_municipios <- read_csv("data-raw/extracao-dos-dados/databases-antigas/tabela_aux_municipios.csv") |>
-  pull(codmunres)
+  pull(municipio)
 
 #Criando um data.frame auxiliar que possui uma linha para cada combinação de município e ano
-df_aux_municipios <- data.frame(codmunres = rep(codigos_municipios, each = length(2012:2022)), ano = 2012:2022)
+df_aux_municipios <- data.frame(codmunres = rep(codigos_municipios, each = length(2022:2023)), ano = 2022:2023)
 
 df_fetais <- df_fetais |> mutate_if(is.character, as.numeric)
 
@@ -425,28 +437,36 @@ df_obitos_perinatais <- juncao %>%
   )
 
 
-write.table(df_obitos_perinatais, 'data-raw/csv/indicadores_bloco7_mortalidade_perinatal_2012-2022.csv', sep = ",", dec = ".", row.names = FALSE)
+write.table(df_obitos_perinatais, 'data-raw/csv/indicadores_bloco7_mortalidade_perinatal_2012-2023.csv', sep = ",", dec = ".", row.names = FALSE)
 
 
 ######### INDICADORES DA ABA A NEONATAL
 
 df_neonat_total <- fetch_datasus(
-  year_start = 2012,
+  year_start = 2022,
   year_end = 2022,
   #vars = c("CODMUNRES", "DTOBITO", "IDADE", "PESO"),
   information_system = "SIM-DOINF"
 )
 
-df_neonat_total2 <- process_sim(df_neonat_total, municipality_data = T) |>
+sim_2023 <- fread("https://s3.sa-east-1.amazonaws.com/ckan.saude.gov.br/SIM/DO23OPEN.csv")
+
+sim23 <- sim_2023 |> select(-c(contador, OPOR_DO, TP_ALTERA, CB_ALT))
+
+df_neonat_total <- df_neonat_total |> select(-c(ESTABDESCR, NUDIASOBIN,
+                                                              NUDIASINF, FONTESINF,
+                                                              CONTADOR))
+
+df_sim_total2 <- process_sim(rbind(df_neonat_total, sim23), municipality_data = T) |>
   select(
     CODMUNRES, DTOBITO, IDADEminutos, IDADEhoras, IDADEdias, PESO
   )
 
 #write.csv(df_neonat_total2, file="Bloco_7/Databases/dados_sim_neonatal.csv")
 
-df_neonat_total3 <- df_neonat_total2  |>
+df_neonat_total3 <- df_sim_total2  |>
   mutate(
-    ano = substr(DTOBITO, 1, 4),
+    ano1 = substr(DTOBITO, 1, 4),
     codmunres = as.numeric(CODMUNRES),
     IDADEminutos = as.numeric(IDADEminutos),
     IDADEhoras = as.numeric(IDADEhoras),
@@ -454,6 +474,10 @@ df_neonat_total3 <- df_neonat_total2  |>
     PESO = as.numeric(PESO)
   ) |>
   mutate(
+    ano = case_when(
+      ano1 == "23" ~ "2023",
+      TRUE ~ ano1
+    ),
     obitos_27dias = case_when(
       (IDADEminutos <= 59| IDADEhoras <= 23 | IDADEdias <= 27) ~ 1,
       !(IDADEminutos <= 59| IDADEhoras <= 23 | IDADEdias <= 27) ~ 0
@@ -583,6 +607,7 @@ df_neonat_total3 <- df_neonat_total2  |>
     )
 
   ) |>
+  select(-c(ano1)) |>
   group_by(codmunres, ano) |>
   summarise(
 
@@ -615,12 +640,18 @@ df_neonat_total3 <- df_neonat_total2  |>
   ungroup()
 
 
-df_nascidos_total <- fetch_datasus(
-  year_start = 2012,
+df_nascidos_total1 <- fetch_datasus(
+  year_start = 2022,
   year_end = 2022,
   vars = c("CODMUNRES", "DTNASC", "PESO"),
   information_system = "SINASC"
 )
+
+sinasc23 <- fread("https://s3.sa-east-1.amazonaws.com/ckan.saude.gov.br/SINASC/DNOPEN23.csv", sep = ";")
+sinasc23 <- sinasc23 |>
+  select(CODMUNRES, DTNASC, PESO)
+
+df_nascidos_total <- rbind(df_nascidos_total1, sinasc23)
 
 df_nascidos_total2 <- process_sinasc(df_nascidos_total, municipality_data = T)
 
@@ -706,7 +737,7 @@ df_neonat <- left_join(df_aux_municipios, df_juncao, by=c("codmunres", "ano")) |
     obitos_1_6dias_mais2500
   )
 
-write.csv(df_neonat, 'data-raw/csv/indicadores_bloco7_mortalidade_neonatal_2012-2022.csv', sep = ",", dec = ".", row.names = FALSE)
+write.csv(df_neonat, 'data-raw/csv/indicadores_bloco7_mortalidade_neonatal_2012-2023.csv', sep = ",", dec = ".", row.names = FALSE)
 
 
 
