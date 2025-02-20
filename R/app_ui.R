c91dff8e42899cbe41efb54ee2f598c43099c4ef
Rafael Herzog <rafasantanah21@gmail.com>
2025-02-20 16:00:01 -0300

    tornando 2023 o ano padrão no nível 1

--- R/app_ui.R
+++ R/app_ui.R
@@ -155,7 +155,7 @@ app_ui <- function(request) {
                   numericInput(
                     inputId = "ano",
                     label = HTML("<span style = 'font-size: 17'> Ano </span>"),
-                    value = 2022,
+                    value = 2023,
                     min = 2012,
                     max = 2024,
                     step = 1,
@@ -303,24 +303,8 @@ app_ui <- function(request) {
                 )
               )
             ),
-            # conditionalPanel(
-            #   condition = "(input.abas == 'nivel_1' & (input.ano == 2023 | input.ano == 2024)) | (input.abas != 'nivel_1' & (input.abas == 'bloco_3' | input.abas == 'bloco_4' | input.abas == 'bloco_5') & (input.ano2[1] == 2023 | input.ano2[1] == 2024))",
-            #   fluidRow(
-            #     column(
-            #       width = 3,
-            #       HTML(
-            #       "
-            #         <div style = 'text-align: left;'> <b style = 'font-size: 15px'>
-            #             <i class='fa-solid fa-circle-info'></i> &nbsp; Os dados de 2023 e 2024 são preliminares
-            #         </b> </div>
-            #         <span style='display: block; margin-bottom: 15px;'> </span>
-            #       "
-            #       )
-            #     )
-            #   )
-            # ),
             conditionalPanel(
-              condition = "((input.abas == 'nivel_1' & input.ano == 2024) | input.abas == 'bloco_1' | input.abas == 'bloco_2' | input.abas == 'bloco_3' | input.abas == 'bloco_4' | input.abas == 'bloco_5' | input.abas == 'bloco_6' | input.abas == 'bloco_7') & input.ano2[1] == 2024",
+              condition = "input.ano == 2024 | input.ano2[1] == 2024",
               fluidRow(
                 column(
                   width = 3,
